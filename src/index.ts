4ba62db4f4756ac7baa8fd8563b53f8fba04278d
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-06-16 16:05:49 +0530

    part three

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express";
 import { AppDataSource } from "./data-source";
 import { Routes } from "./routes";
 import * as morgan from "morgan";
+import { validationResult } from "express-validator/src/validation-result";
 import { port } from "./config";
 
 function handleError(err, _req, res, _next) {
@@ -21,8 +22,13 @@ AppDataSource.initialize()
     Routes.forEach((route) => {
       (app as any)[route.method](
         route.route,
+        ...route.validation,
         async (req: Request, res: Response, next: Function) => {
           try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+              return res.status(400).json({ errors: errors.array() });
+            }
             const result = await new (route.controller as any)()[route.action](
               req,
               res,
--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,37 @@
-import { UserController } from "./controller/UserController"
+import { body, param } from "express-validator";
+import { UserController } from "./controller/UserController";
 
-export const Routes = [{
+export const Routes = [
+  {
     method: "get",
     route: "/users",
     controller: UserController,
-    action: "all"
-}, {
+    action: "all",
+    validation: [],
+  },
+  {
     method: "get",
     route: "/users/:id",
     controller: UserController,
-    action: "one"
-}, {
+    action: "one",
+    validation: [param("id").isInt().withMessage("id must be integer")],
+  },
+  {
     method: "post",
     route: "/users",
     controller: UserController,
-    action: "save"
-}, {
+    action: "save",
+    validation: [
+      body("firstName").isString().withMessage("firstName must be string"),
+      body("lastName").isString().withMessage("lastName must be string"),
+      body("age").isInt({ min: 0 }).withMessage("age must be integer"),
+    ],
+  },
+  {
     method: "delete",
     route: "/users/:id",
     controller: UserController,
-    action: "remove"
-}]
+    action: "remove",
+    validation: [param("id").isInt().withMessage("id must be integer")],
+  },
+];
