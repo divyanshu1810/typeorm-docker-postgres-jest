1aad275882e873e31fdc37dec3eea9c2d3cae5d4
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-06-16 12:57:58 +0530

    part two

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,44 @@ import * as bodyParser from "body-parser";
 import { Request, Response } from "express";
 import { AppDataSource } from "./data-source";
 import { Routes } from "./routes";
-import { User } from "./entity/User";
+import * as morgan from "morgan";
+import { port } from "./config";
+
+function handleError(err, _req, res, _next) {
+  res.status(err.statusCode || 500).send({ message: err.message });
+}
 
 AppDataSource.initialize()
   .then(async () => {
     // create express app
     const app = express();
+    app.use(morgan("tiny"));
     app.use(bodyParser.json());
 
     // register express routes from defined application routes
     Routes.forEach((route) => {
       (app as any)[route.method](
         route.route,
-        (req: Request, res: Response, next: Function) => {
-          const result = new (route.controller as any)()[route.action](
-            req,
-            res,
-            next
-          );
-          if (result instanceof Promise) {
-            result.then((result) =>
-              result !== null && result !== undefined
-                ? res.send(result)
-                : undefined
+        async (req: Request, res: Response, next: Function) => {
+          try {
+            const result = await new (route.controller as any)()[route.action](
+              req,
+              res,
+              next
             );
-          } else if (result !== null && result !== undefined) {
             res.json(result);
+          } catch (error) {
+            next(error);
           }
         }
       );
     });
 
-    // setup express app here
-    // ...
-
     // start express server
-    app.listen(3000);
+    app.use(handleError);
+    app.listen(port);
     console.log(
-      "Express server has started on port 3000. Open http://localhost:3000/users to see results"
+      `Express server has started on port ${port}. Open http://localhost:${port}/users to see results`
     );
   })
   .catch((error) => console.log(error));
